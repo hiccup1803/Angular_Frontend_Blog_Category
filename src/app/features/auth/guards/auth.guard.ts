847e99b0a7e77774f67ba5310506f6ae875afafd
hiccup <hiccup1803@gmail.com>
2024-01-09 14:09:10 -0500

    1_9

--- src/app/features/auth/guards/auth.guard.ts
+++ src/app/features/auth/guards/auth.guard.ts
@@ -1,5 +1,45 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../services/auth.service';
+import { jwtDecode } from 'jwt-decode';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const cookieService = inject(CookieService);
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getUser();
+
+  // Check for the JWT Token
+  let token = cookieService.get('Authorization');
+
+  if(token && user) {
+    token = token.replace('Bearer ', '');
+    const decodedtoken: any = jwtDecode(token);
+
+    // Check if the token has expired
+    const expirationDate = decodedtoken.exp * 1000;
+    const currentTime = new Date().getTime();
+
+    if(expirationDate < currentTime) {
+      // Logout
+      authService.logout();
+      return router.createUrlTree(['/login'], {queryParams: { returnUrl: state.url}});
+    }
+    else {
+      // Token is still valid
+      if (user.roles.includes('Writer')) {
+        return true;
+      }
+      else {
+        alert('Unauthorized');
+        return false;
+      }
+    }
+  }
+  else {
+    // Logout
+    authService.logout();
+    return router.createUrlTree(['/login'], {queryParams: { returnUrl: state.url}});
+  }
 };
